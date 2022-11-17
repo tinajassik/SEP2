d6740f5788315ad42ddc68450041b875c7f849dd
aleadamczak <ola.adamczak4111@gmail.com>
2022-11-17 11:04:32 +0100

    Aleksandra pt1

---
+++ src/client/shared/view/createAccountView/CreateAccountViewController.java
@@ -0,0 +1,47 @@
+package client.shared.view.createAccountView;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CreateAccountViewController
+{
+  @FXML
+  private TextField fullNameField;
+  @FXML
+  private TextField dateOfBirthField;
+  @FXML
+  private TextField addressField;
+  @FXML
+  private TextField phoneNumberField;
+  @FXML
+  private TextField emailField;
+  @FXML
+  private TextField usernameField;
+  @FXML
+  private TextField passwordField;
+
+  private CreateAccountViewModel createAccountViewModel;
+
+  public void init(CreateAccountViewModel createAccountViewModel)
+  {
+    this.createAccountViewModel = createAccountViewModel;
+    this.fullNameField.textProperty().bindBidirectional(createAccountViewModel.fullNameProperty());
+    this.dateOfBirthField.textProperty().bindBidirectional(createAccountViewModel.dateOfBirthProperty());
+    this.addressField.textProperty().bindBidirectional(createAccountViewModel.addressProperty());
+    this.phoneNumberField.textProperty().bindBidirectional(createAccountViewModel.phoneNumberProperty());
+    this.emailField.textProperty().bindBidirectional(createAccountViewModel.emailProperty());
+    this.usernameField.textProperty().bindBidirectional(createAccountViewModel.usernameProperty());
+    this.passwordField.textProperty().bindBidirectional(createAccountViewModel.passwordProperty());
+  }
+
+  public void onRegisterSeller(ActionEvent actionEvent)
+  {
+
+  }
+
+  public void onRegisterBuyer(ActionEvent actionEvent)
+  {
+
+  }
+}
---
+++ src/client/shared/view/createAccountView/CreateAccountViewModel.java
@@ -0,0 +1,35 @@
+package client.shared.view.createAccountView;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class CreateAccountViewModel
+{
+  private StringProperty fullName, dateOfBirth, address, phoneNumber, email, username, password;
+
+  public CreateAccountViewModel()
+  {
+    this.fullName = new SimpleStringProperty();
+    this.dateOfBirth = new SimpleStringProperty();
+    this.address = new SimpleStringProperty();
+    this.phoneNumber = new SimpleStringProperty();
+    this.email = new SimpleStringProperty();
+    this.username = new SimpleStringProperty();
+    this.password = new SimpleStringProperty();
+  }
+
+  public StringProperty fullNameProperty(){return fullName;}
+
+  public StringProperty dateOfBirthProperty(){return dateOfBirth;}
+
+  public StringProperty addressProperty(){return address;}
+
+  public StringProperty phoneNumberProperty(){return phoneNumber;}
+
+  public StringProperty emailProperty(){return email;}
+
+  public StringProperty usernameProperty(){return username;}
+
+  public StringProperty passwordProperty(){return password;}
+
+}
