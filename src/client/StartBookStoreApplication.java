760ab2e4626e7085ccda06805a9315d9d5a776e7
Kristina Jassova <kristinka.jassova@gmail.com>
2022-11-22 16:39:58 +0100

    Main View for Buyers - beginning lesgoo

---
+++ src/client/StartBookStoreApplication.java
@@ -0,0 +1,9 @@
+package client;
+
+import javafx.application.Application;
+
+public class StartBookStoreApplication {
+    public static void main(String[] args) {
+        Application.launch(BookStoreApplication.class);
+    }
+}
---
+++ src/client/views/buyer/mainPageView/MainPageViewModel.java
@@ -0,0 +1,17 @@
+package client.views.buyer.mainPageView;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class MainPageViewModel {
+
+//    private StringProperty fullName;
+//    private StringProperty username;
+//
+//    public MainPageViewModel() {
+//        fullName = new SimpleStringProperty();
+//        username = new SimpleStringProperty();
+//    }
+
+
+}
